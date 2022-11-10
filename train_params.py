d3686fa5741af643a5ac398290edd142cd4e6914
BrendanOConnor <brendan242424@gmail.com>
2022-11-10 23:09:18 +0000

    Changed params

--- train_params.py
+++ train_params.py
@@ -14,7 +14,7 @@ if use_avg_singer_embs: embs_string = 'avgEmbs'
 else: embs_string = 'liveEmbs'
 
 
-include_code_loss = True # dsicvoered this is necessary for good vc, so leave it in from now on
+include_code_loss = False # dsicvoered this is necessary for good vc, so leave it in from now on
 if include_code_loss: ccloss_string = 'CcLoss'
 else: ccloss_string = ''
 use_emb_loss = False
@@ -30,7 +30,7 @@ pitch_dir = '/import/c4dm-02/bdoc3/world_data/damp_80_16ms' #it doesn't matter w
 SIE_pitch_cond = False
 if SIE_pitch_cond: siePitchCond = 'siePitchCond'
 else: siePitchCond = ''
-SVC_pitch_cond = True
+SVC_pitch_cond = False
 if SVC_pitch_cond: svcPitchCond = 'svcPitchCond'
 else: svcPitchCond = ''
 
@@ -41,18 +41,19 @@ else: norm_method_string = ''
 
 patience = 1000000 # choose 20 when considering damp dataset
 patience_thresh = 0.0 #0.1 for damp dataset
+early_stopping_loss = 'total' # 'recon', 'cc', 'sie', 'total'
 
 training_fn = f'{embs_string}_{ccloss_string}{embloss_string}{classloss_string}_{siePitchCond}{svcPitchCond}_{norm_method_string}'
 
 
 SVC_models_dir = '/homes/bdoc3/my_data/autovc_models/autoSvc'
 # SIE_model_path = '/homes/bdoc3/my_data/autovc_models/singer-identity-encoder/autoVc_pretrainedOnVctk_Mels80'
-# SIE_model_path = '/homes/bdoc3/my_data/autovc_models/singer-identity-encoder/bestPerformingSIE_mel80'
-SIE_model_path = '/homes/bdoc3/my_data/autovc_models/singer-identity-encoder/sie_VoxCeleb1LibrispeechDampQianParams_mels80'
-# svc_ckpt_path = os.path.join(SVC_models_dir, 'vctk_mel_Size1.0-avgEmbs_WithCcLoss-autoVc_pretrainedOnVctk_Mels80-trainUpTo300k', 'saved_model.pt')
-svc_ckpt_path = ''
-pkl_fn_extras = '_100avg'
-fn_final_notes = ''
+SIE_model_path = '/homes/bdoc3/my_data/autovc_models/singer-identity-encoder/bestPerformingSIE_mel80'
+# SIE_model_path = '/homes/bdoc3/my_data/autovc_models/singer-identity-encoder/sie_VoxCeleb1LibrispeechDampQianParams_mels80'
+svc_ckpt_path = os.path.join(SVC_models_dir, 'damp_mel_Size0.25-avgEmbs_with-bestPerformingSIE_mel80-', 'ckpt_500000.pt')
+# svc_ckpt_path = ''
+pkl_fn_extras = ''
+fn_final_notes = 'Cont2'
 
 SVC_model_name = f'{ds_fn}-{training_fn}-{os.path.basename(SIE_model_path)}-{fn_final_notes}'
 # SVC_model_name = 'damp_freq32QuarterDataset_10EmbAverage_patience20Thresh.1-bestPerformingSIE_mel80'
@@ -61,11 +62,11 @@ print('Model name: ', SVC_model_name)
 
 
 # fill this out only if you want SVC input features to be different to SIE input features
-# SVC_feat_dir = '/import/c4dm-02/bdoc3/world_data/damp_80_16ms'
-SVC_feat_dir = f'/import/c4dm-02/bdoc3/spmel/{use_loader}_qianParams'
-# SIE_feat_dir = f'/import/c4dm-02/bdoc3/spmel/{use_loader}_qianParams' #always MEL-type if using bestPerformingSIE
+SVC_feat_dir = '/import/c4dm-02/bdoc3/world_data/damp_80_16ms'
+# SVC_feat_dir = f'/import/c4dm-02/bdoc3/spmel/{use_loader}_qianParams'
+SIE_feat_dir = f'/import/c4dm-02/bdoc3/spmel/{use_loader}_qianParams' #always MEL-type if using bestPerformingSIE
 # SVC_feat_dir = '/homes/bdoc3/my_data/spmel_data/vocadito'
-SIE_feat_dir = SVC_feat_dir
+# SIE_feat_dir = SVC_feat_dir
 
 use_ckpt_config = False
 #SVC params
@@ -94,10 +95,10 @@ midi_range = range(36, 82) # should be the same as used in SIE if trained with p
 # schedulers
 # ckpt_freq = 1000
 log_step = 10
-max_iters = 500001 #measured in batches, not data examples
+max_iters = 1000000 #measured in batches, not data examples
 max_cycle_iters = 1000
-ckpt_freq = 500000 #make this number higher than max_iters to avoid ckpts altogether
-spec_freq = 10000
+ckpt_freq = 1000000 #make this number higher than max_iters to avoid ckpts altogether
+spec_freq = 50000
 
 
 
